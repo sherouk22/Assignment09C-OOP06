8617634e20d5eb9242f65a666869c2c022ba7ff8
sherouk22 <sheroukmohy558@gmail.com>
2025-01-25 22:21:47 +0200

    Add project files.

---
+++ Assignment09C#OOP06/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Assignment09C_OOP06
+{
+    internal class Program
+    {//Assignment09C#OOP06
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
