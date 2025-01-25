918ed8ea800ef74b5ff8cbf54b8f214398df27eb
sherouk22 <sheroukmohy558@gmail.com>
2025-01-26 01:11:38 +0200

    Assignment09C#OOP06

---
+++ Assignment09C#OOP06/Duration.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment09C_OOP06
+{
+    public class Duration
+    {
+       
+
+        public int? Hours {  get; set; }
+        
+        public int? Minutes { get; set; }
+
+        public int? Seconds { get; set; }
+
+        public Duration(int hours, int minutes, int seconds)
+        {
+            Hours = hours;
+            Minutes = minutes;
+            Seconds = seconds;
+        }
+
+        public Duration()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return $"hours: {Hours} ,minutes: {Minutes} ,seconds: {Seconds} ";
+
+        }
+
+        public Duration(int totalSeconds)
+        {
+            Hours = totalSeconds/3600;
+            totalSeconds %= 3600;
+            Minutes = totalSeconds / 60;
+            Seconds = totalSeconds % 60;
+
+        }
+
+        public override bool Equals(object? obj)
+        {
+            Duration duration = (Duration)obj;
+
+            return Hours == duration.Hours && Minutes == duration.Minutes && Seconds == duration.Seconds;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Hours, Minutes, Seconds);
+        }
+
+        public static Duration operator + (Duration left , Duration right)
+        {
+            return new Duration
+            {
+                Hours = left.Hours + right.Hours,
+                Minutes = left.Minutes + right.Minutes,
+                Seconds = left.Seconds + right.Seconds
+            };
+        }
+
+        
+         public static Duration operator + (Duration left ,int seconds)
+        {
+            return new Duration
+            {
+                Hours = left.Hours,
+                Minutes = left.Minutes,
+                Seconds = left.Seconds + seconds
+
+            };
+        }
+
+        public static Duration operator +(int seconds, Duration right)
+        {
+            return new Duration
+            {
+                Hours = right.Hours,
+                Minutes = right.Minutes,
+                Seconds = right.Seconds + seconds
+
+            };
+
+        }
+
+        public static Duration operator ++(Duration duration)
+        {
+            return new Duration 
+            {
+                Hours = duration.Hours,
+                Minutes = duration.Minutes +1 ,
+                Seconds = duration.Seconds
+            };
+        }
+
+        public static Duration operator --(Duration duration)
+        {
+            return new Duration
+            {
+                Hours = duration.Hours,
+                Minutes = duration.Minutes - 1,
+                Seconds = duration.Seconds
+            };
+        }
+
+        public static bool operator > (Duration left, Duration right)
+        {
+            if (left.Hours == right.Hours)
+                return left.Minutes > right.Minutes;
+            else if (left.Minutes == right.Minutes)
+                return left.Seconds > right.Seconds;
+            else
+                return right.Hours > left.Hours;
+
+
+        }
+
+        public static bool operator < (Duration left, Duration right)
+        {
+            if (left.Hours == right.Hours)
+                return left.Minutes < right.Minutes;
+            else if (left.Minutes == right.Minutes)
+                return left.Seconds < right.Seconds;
+            else
+                return right.Hours < left.Hours;
+
+
+        }
+
+        public static bool operator <=(Duration left, Duration right)
+        {
+            return !(left > right);
+        }
+
+        public static bool operator >=(Duration left, Duration right)
+        {
+            return !(left < right);
+        }
+
+    }
+}
--- Assignment09C#OOP06/Program.cs
+++ Assignment09C#OOP06/Program.cs
@@ -4,7 +4,51 @@
     {//Assignment09C#OOP06
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+
+            Duration D1 = new Duration(1, 10, 15);
+            Console.WriteLine(D1.ToString());
+
+
+            Duration D2 = new Duration(1, 30, 15);
+            Console.WriteLine(D1.ToString());
+
+            // Duration D1 = new Duration(3600);
+            // Console.WriteLine(D1.ToString());
+
+            // Duration D2 = new Duration(7800);
+            // Console.WriteLine(D2.ToString());
+
+            // Duration D3 = new Duration(666);
+            //Console.WriteLine(D3.ToString());
+
+
+
+            //Duration D3 = D2 + D1;
+
+            //Console.WriteLine(D3.ToString());
+
+            //Duration D3 = D2 + 14566;
+
+            //Console.WriteLine(D3.ToString());
+
+            //  Duration D3 = D1+ 14566;
+
+            //Console.WriteLine(D3.ToString());
+
+            //    Duration D3 = ++D2;
+            //    Duration D4 = --D2;
+            //    Console.WriteLine(D3.ToString());
+            //    Console.WriteLine(D4.ToString());
+
+            if (D1 > D2)
+                Console.WriteLine("D1 is greater than D2");
+            else if (D1 < D2)
+            
+                Console.WriteLine("D1 is less than  to D2");
+            
+            else
+                Console.WriteLine("D1 is equal to D2");
         }
     }
 }
